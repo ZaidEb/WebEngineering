05ab6d1994956b59789ebf965ca044007233689b
Zaid Ebrahimkhil <zaid.ebrahimkhil@students.fhnw.ch>
2025-11-23 21:59:43 +0100

    Add ServiceCards component to Home page

--- formalweb/src/app/page.tsx
+++ formalweb/src/app/page.tsx
@@ -1,5 +1,12 @@
 import Hero from "../components/Hero";
+import ServiceCards from "../components/ServiceCards";
+import { services } from "../data/services";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <ServiceCards services={services} />
+    </>
+  );
 }
